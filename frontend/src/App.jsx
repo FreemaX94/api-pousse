d7290735790e8c588f485e147986e6ac2ba71aed
FreemaX94 <168763795+FreemaX94@users.noreply.github.com>
2025-06-03 23:38:00 +0200

    Add password reset routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,22 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import NavBar from './components/NavBar';
 
-import Login         from './pages/Login';
-import Signup        from './pages/Signup';
-import Home          from './Home';
-import Evenements    from './pages/Evenements';
-import Creation      from './pages/Creation';
-import Entretien     from './pages/Entretien';
-import AddContract   from './pages/AddContract';
-import Depot         from './pages/Depot';
-import Vehicules     from './pages/Vehicules';
-import Statistiques  from './pages/Statistiques';
-import Parametres    from './pages/Parametres';
-import Comptabilite  from './pages/Comptabilite';
-import LivraisonList from './pages/LivraisonList';
+import Login          from './pages/Login';
+import Signup         from './pages/Signup';
+import Home           from './Home';
+import Evenements     from './pages/Evenements';
+import Creation       from './pages/Creation';
+import Entretien      from './pages/Entretien';
+import AddContract    from './pages/AddContract';
+import Depot          from './pages/Depot';
+import Vehicules      from './pages/Vehicules';
+import Statistiques   from './pages/Statistiques';
+import Parametres     from './pages/Parametres';
+import Comptabilite   from './pages/Comptabilite';
+import LivraisonList  from './pages/LivraisonList';
 import ActivationPage from './pages/ActivationPage';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword  from './pages/ResetPassword';
 
 import PrivateRoute  from './components/PrivateRoute';
 import TestSanitize  from './components/TestSanitize';
@@ -33,8 +35,10 @@ export default function App() {
           <Route path="/" element={<Navigate to="/login" replace />} />
           
           {/* Auth routes */}
-          <Route path="/login"    element={<Login />} />
-          <Route path="/signup"   element={<Signup />} />
+          <Route path="/login"           element={<Login />} />
+          <Route path="/signup"          element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password"  element={<ResetPassword />} />
           <Route path="/activate/:token" element={<ActivationPage />} />
 
           {/* Protected routes */}
